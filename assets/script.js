ba03eb13e2768a955f7a5cbccf5736e7a50e46a6
feras <feras.altalib@gmail.com>
2024-02-21 13:48:54 +0100

    1

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ let tarifNormal = document.getElementById("tarifNormal");
 let popUpTarifReduit = document.querySelector(".card");
 let daccordBoutonTarifReduit = document.querySelector(".DaccordBouton");
 let reservationBouton = document.getElementById("reservationBouton");
+let alertOption = document.getElementById("alertOption");
 let optionBouton = document.getElementById("optionBouton");
 let pass1jour = document.getElementById("pass1jour");
 let pass2jours = document.getElementById("pass2jours");
@@ -13,10 +14,13 @@ let reservationSection = document.getElementById("reservation");
 let optionsSection = document.getElementById("options");
 let coordonneesSection = document.getElementById("coordonnees");
 
-console.log(pass1jour);
+let NombrePlaces = document.getElementById("NombrePlaces");
+
 console.log(tarifNormal);
 tarifReduit.addEventListener("change", function () {
+  console.log(NombrePlaces.value);
   if (tarifReduit.checked) {
+    alertOption.style.display = "none";
     popUpTarifReduit.style.display = "block";
     SectionTarifReduit.style.display = "block";
     SectionTarifNormal.style.display = "none";
@@ -30,6 +34,7 @@ tarifReduit.addEventListener("change", function () {
 });
 tarifNormal.addEventListener("change", function () {
   if (tarifNormal.checked) {
+    alertOption.style.display = "none";
     popUpTarifReduit.style.display = "none";
     SectionTarifReduit.style.display = "none";
     SectionTarifNormal.style.display = "block";
@@ -44,12 +49,29 @@ daccordBoutonTarifReduit.addEventListener("click", function () {
   reservationBouton.style.display = "block";
 });
 
+function chooseTariff() {
+  if (!tarifReduit.checked && !tarifNormal.checked) {
+    alertOption.textContent = "Choisissez un tarif";
+    return false;
+  }
+  return true;
+}
 reservationBouton.addEventListener("click", function () {
-  reservationSection.style.display = "none";
-  optionsSection.style.display = "block";
+  if (chooseTariff() && NombrePlaces.value > 0) {
+    reservationSection.style.display = "none";
+    optionsSection.style.display = "block";
+  }
+  
 });
 
 optionBouton.addEventListener("click", function () {
   optionsSection.style.display = "none";
   coordonneesSection.style.display = "flex";
+});
+
+let nombrePlacesValue = 0;
+
+NombrePlaces.addEventListener("change", function () {
+  nombrePlacesValue = parseInt(NombrePlaces.value);
+  console.log(nombrePlacesValue);
 });
