f369389cd272fe7868430613b45209985eff5d24
feras <feras.altalib@gmail.com>
2024-02-29 15:13:57 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -9,24 +9,14 @@
   <script src="./assets/script.js" async></script>
 </head>
 <div>
-<<<<<<< HEAD
       <?php if (isset($_SESSION['connecté'])) { ?>
         <a href="deconnexion.php">Déconnexion</a>
       <?php } else { ?>
-        <a href="connexion.php">Connexion</a>
-      <?php } ?>
-    </div>
-=======
-  <?php if (isset($_SESSION['connecté'])) { ?>
-    <a href="deconnexion.php">Déconnexion</a>
-  <?php } else { ?>
     <header>
       <a href="https://www.vercorsmusicfestival.com/" class="header__logo">
         <img src="../logo.png" class="logo" alt="Vercors Music Festival">
       </a>
       <button><a href="connexion.php" class="link">Connexion</a></button>
-      <button><a href="index.php" class="link">Inscription</a></button>
     </header>
   <?php } ?>
-</div>
->>>>>>> a801064d35629e09dab3b00521b6c9da0202dc57
+    </div>
